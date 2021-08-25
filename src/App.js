083ba9e3496abfb0b282f8b74610ca6c8232ce91
Marco <james.marco.reyes@eee.upd.edu.ph>
2021-08-25 23:13:52 +0800

    navbar testing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import { Navbar } from './Navbar';
+import { Navbar } from './components';
 
 function App() {
   return (
     <div className="App">
-      <h1>hiiii</h1>
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Nav } from './Navbar.elements';
+
+function Navbar() {
+    return (
+        <>
+            <Nav>
+                <NavbarContainer></NavbarContainer>
+            </Nav>
+        </>
+    )
+}
+
+export default Navbar
