7bb2223468b7ae73f84ff992f1213cd4d011d59b
Marco <james.marco.reyes@eee.upd.edu.ph>
2021-08-26 15:06:09 +0800

    navitem

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -71,6 +71,22 @@ export const NavMenu = styled.ul`
   }
 `;
 
+export const NavItem = styled.li`
+  height: 80px;
+  border-bottom: 2px solid transparent;
+
+  &:hover {
+    border-bottom: 2px solid #4b59f7;
+  }
+
+  @media screen and (max-width: 960px) {
+    width: 100%;
+
+    &:hover {
+      border: none;
+    }
+  }
+`;
 
 
 
