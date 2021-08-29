d01aad9facf14a7f6a1b24649673f84c49c82719
Marco <james.marco.reyes@eee.upd.edu.ph>
2021-08-29 13:55:09 +0800

    shotbutton & useEffect

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,25 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import { IconContext } from 'react-icons/lib';
-import { Nav, 
-         NavbarContainer,
-         NavLogo,
-         NavIcon,
-         MobileIcon,
-         NavMenu,
-         NavItem,
-         NavLinks } from './Navbar.elements';
+import { Button } from '../../globalStyles';
+import {
+    Nav,
+    NavbarContainer,
+    NavLogo,
+    NavIcon,
+    MobileIcon,
+    NavMenu,
+    NavItem,
+    NavItemBtn,
+    NavLinks,
+    NavBtnLink
+  } from './Navbar.elements';
 
 
 
 function Navbar() {
     //sets the value of click and setclick to false
     const [click, setClick] = useState(false)
+    const [button, setButton] = useState(true);
 
     //MobileIcon
     //function that updates the status of the click, and toggles the button
     const handleClick = () => setClick(!click) //starts the setClick fxn as unclicked
-        
+    const closeMobileMenu = () => setClick(false);
+    
+    const showButton = () => {
+        if (window.innerWidth <= 960) {
+            setButton(false);
+        } else {
+            setButton(true);
+        }
+    };
+
+    useEffect(() => {
+        showButton();
+      }, []);
+    
+    window.addEventListener('resize', showButton);
     
     return (
         <>
@@ -55,11 +75,17 @@ function Navbar() {
                                 </NavLinks>
                             </NavItem>
 
-                            {/* Naviten button */}
+                            {/* Navitem button */}
                             <NavItemBtn>
                                 {button ? (
                                     <NavBtnLink to="/sign-up">
                                         <Button primary>SIGN UP</Button>
+                                    </NavBtnLink>
+                                ) : (
+                                    <NavBtnLink to="/sign-up">
+                                        <Button onClick = {closeMobileMenu} fontBig primary>
+                                            SIGN UP
+                                        </Button>
                                     </NavBtnLink>
                                 )}
                             </NavItemBtn>
