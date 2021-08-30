704fbbb9d6253a0ff751998e8be6c7704f65f30a
Marco <james.marco.reyes@eee.upd.edu.ph>
2021-08-30 22:52:02 +0800

    Infosection init & style

---
+++ src/components/InfoSection/InfoSection.element.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const InfoSec = style.div`
+    color: #fff;
+    padding: 160px 0;
+    background: ${({ lightBg }) => (lightBg ? '#fff' : '#101522')};
+    
+
+`
